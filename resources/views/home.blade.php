8cfcda31d1f6923e1f4ade78a325cf703a98ee3f
Prog <prog64650@gmail.com>
2025-07-07 13:42:23 +0500

    1.0.2 commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -19,7 +19,7 @@
         <div class="row">
             <div class="col-3 bg-light p-3 rounded-4">
                 <h4 class="pb-2 fw-bold d-flex justify-content-between">Menu<i class="bi bi-list"></i></h4>
-                <input class="form-control mr-sm-2 bg-light" type="text" placeholder="Search" aria-label="Search">
+                <input class="form-control bg-light" type="text" placeholder="Search" aria-label="Search">
                 <div>
                     <div class="fw-bolder pt-4 small text-muted">TASKS</div>
                     <div class="task-link py-2 rounded-3"><a href="#" style="text-decoration: none" class="text-secondary"><i class="bi bi-chevron-double-right mx-2"></i>Calendar</a></div>
@@ -39,12 +39,29 @@
                 <div class="pb-5 mb-5">
                     <div class="fw-bolder small text-muted mb-2">TAGS</div>
                     <div class="d-flex gap-2">
-                        <div class="px-3 py-1 bg-body-secondary rounded-3 fw-semibold">Tag 1</div>
-                        <div class="px-3 py-1 bg-body-secondary rounded-3 fw-semibold">Tag 2</div>
-                        <div class="px-3 py-1 bg-body-secondary rounded-3 fw-semibold">+ Add Tag</div>
+                        <div class="px-3 py-1 bg-body-secondary rounded-3 fw-semibold"><a href="#" style="text-decoration: none" class="text-dark" a>Tag 1</a></div>
+                        <div class="px-3 py-1 bg-body-secondary rounded-3 fw-semibold"><a href="#" style="text-decoration: none" class="text-dark" a>Tag 2</a></div>
+                        <div class="px-3 py-1 bg-body-secondary rounded-3 fw-semibold"><a href="#" style="text-decoration: none" class="text-dark" a>+ Add Tag</a></div>
                     </div>
                 </div>
-                <a href="#" style="text-decoration: none" class="text-secondary"><i class="bi bi-sliders2"></i>Settings</a>
+                <div class="task-link py-2 rounded-3 h6"><a href="#" style="text-decoration: none; font-size: 17px" class="text-secondary"><i class="bi bi-sliders2 mx-2"></i>Settings</a></div>
+                <div class="task-link py-2 rounded-3 h6"><a href="#" style="text-decoration: none; font-size: 17px" class="text-secondary"><i class="bi bi-box-arrow-right mx-2"></i>Sign out</a></div>
+            </div>
+            <div class="col-6 ps-4">
+                <div class="d-flex align-items-center mb-3">
+                    <h1 class="me-5 fw-bold">Today</h1>
+                    <span class="h3 border rounded px-2 py-1 fw-semibold">5</span>
+                </div>
+                <a href="#" style="text-decoration: none" class="text-secondary border rounded-2 form-control ps-3"><span class="h4">+</span> Add New List</a>
+                <div class="d-flex justify-content-between pt-3 ">
+                    <div class="form-check ps-5 fw-semibold">
+                        <input class="form-check-input" type="checkbox" id="">
+                        <label class="ps-2">Research content ideas</label>
+                    </div>
+                    <i class="bi bi-chevron-right h5 pe-4"></i>
+                </div>
+                <hr>
+
             </div>
         </div>
     </div>
